8b7681c536dd4373861775fcdc75eef9627bb5ef
Cristopher Guerrero <axl_699@hotmail.com>
2017-01-02 14:01:35 -0600

    0.1.1
    Compilando el programa

--- juego C++.cpp
+++ juego C++.cpp
@@ -5,8 +5,20 @@ int main ()
 {
     printf("*");
 
+    //Tipo handle para controlar punteros de manera abstracta
     HANDLE hCon;
-    hCon = GetStdHandle();
 
-    SetConsoleCursorPosition();
+    //Sirve para obtener el puntero de este programa y guardarlo en una variable
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    //Se define un objeto de tipo coordenada y se definen posiciones.
+    COORD dwPos;
+    dwPos.X = 4;
+    dwPos.Y = 5;
+
+    //Posiciona el cursor de la consola con el primer parámetro que es el programa y como segundo el objeto que contiene las coordenadas.
+    SetConsoleCursorPosition(hCon, dwPos);
+    printf("*");
+
+    return 0;
 }
